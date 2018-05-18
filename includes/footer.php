37e2ef259e70f400beb0c8dee8496a22708f86cb
Kumar Anurag <kmranrg@gmail.com>
2018-05-19 01:44:21 +0530

    added header and footer

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div style="background: #2d2d2d;">
+	<div class="container" style="color: white">
+		<center>
+			<p>Copyright &copy; Internshala | All Rights Reserved</p>
+		</center>
+	</div>
+</div>
