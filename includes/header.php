aa0a0835aff0133f6b7cb4cc605c1eebe25487ca
Kumar Anurag <kmranrg@gmail.com>
2018-05-19 05:44:37 +0530

    login 3D pop-up effect and registration page

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,67 @@
 <div class="jumbotron" style="position: relative">
 	<div class="container">
-		<img src="images/logo.png" height="80px" width="200px" style="margin-left: 20px">
-		<div style="float: right;display: inline-flex;position: absolute; right: 3vw; top: 50%;transform: translate(0,-50%);">
-			<button type="button" class="btn btn-primary">Login</button>&nbsp;&nbsp;
+		<img src="img/logo.png" height="80px" width="200px" style="margin-left: 20px">
+		<div class="btn-right">
+			<button type="button" id="Login-toggle" class="btn btn-info">Login</button>&nbsp;&nbsp;
 			<div class="dropdown">
 				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
  					Register
   					<span class="caret"></span>
   				</button>
+			
 				<ul class="dropdown-menu">
-					<li><a href="#">As Student</a>
+					<li><a href="stu_register.php">As Student</a>
+					
 					</li>
-					<li><a href="#">As Employee</a>
+					<li><a href="emp_register.php">As Employee</a>
+					
 					</li>
 				</ul>
 			</div>
+		</div>
+	</div>
+</div>
+<div class="overlay">
+
+</div>
+
+<div class="jumbotron" id="login-pane">
+	<div class="container">
+		<ul class="nav nav-pills justify-content-center">
+			<li class="nav-item">
+				<label for="std-login" class="nav-link active" id="std">Student</label>
+				<input type="radio" name="usr-category" class="hidden" value="student" id="std-login">
+
+			</li>
+			<li class="nav-item">
+				<label for="emp-login" class="nav-link" id="emp">Employee</label>
+				<input type="radio" class="hidden" name="usr-category" value="employee" id="emp-login">
+			</li>
+			<li class="nav-item">
+		</ul>
+		<br>
+	</div>
+	<button type="button" class="login-close" style="position: absolute;top:10px;right:10px;">
+        <span aria-hidden="true">&times;</span>
+      </button>
+
+
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text">@</span>
+		</div>
+		<input type="text" class="form-control" id="login-email" placeholder="Student email">
+	</div>
+	<div class="input-group mb-3">
+		<div class="input-group-prepend">
+			<span class="input-group-text"><img src="img/key.svg" height="15px" > </span>
+		</div>
+		<input type="text" class="form-control" placeholder="Password">
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10">
+			<center><button type="submit" class="btn btn-primary">Sign in</button>
+			</center>
 		</div>
 	</div>
 </div>
--- index.php
+++ index.php
@@ -2,18 +2,14 @@
 <html>
 
 <head>
-	<meta http-equiv="Refresh" content="3">
 	<meta charset="utf-8">
 	<title>Internshala</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/js" href="js/bootstrap.min.js">
-	<link rel="stylesheet" type="text/js" href="js/jquery.js">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="css/master.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
+	<style>
+		<?php include 'css/master.css';
+		?>
+	</style>
 
 </head>
 
@@ -23,13 +19,349 @@
 		include("includes/header.php"); 
 	?>
 
+	<div class="row text-center" id="cameras">
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/5.jpg" alt="">
+				<div class="caption">
+					<h3>Cannon EOS </h3>
+					<p>Price: Rs. 36000.00 </p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 1 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=1" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+				</div>
+			</div>
+		</div>
 
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/2.jpg" alt="">
+				<div class="caption">
+					<h3>Samsung EOS </h3>
+					<p>Price: Rs. 40,000.00 </p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 2 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=2" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/3.jpg" alt="">
+				<div class="caption">
+					<h3>Sony DSLR</h3>
+					<p>Price: Rs. 50000.00</p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 3 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=3" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/4.jpg" alt="">
+				<div class="caption">
+					<h3>Olympus DSLR</h3>
+					<p>Price: Rs. 50000.00</p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 4 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=4" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/5.jpg" alt="">
+				<div class="caption">
+					<h3>Cannon EOS </h3>
+					<p>Price: Rs. 36000.00 </p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 1 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=1" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/2.jpg" alt="">
+				<div class="caption">
+					<h3>Samsung EOS </h3>
+					<p>Price: Rs. 40,000.00 </p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 2 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=2" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/3.jpg" alt="">
+				<div class="caption">
+					<h3>Sony DSLR</h3>
+					<p>Price: Rs. 50000.00</p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 3 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=3" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/4.jpg" alt="">
+				<div class="caption">
+					<h3>Olympus DSLR</h3>
+					<p>Price: Rs. 50000.00</p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 4 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=4" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/5.jpg" alt="">
+				<div class="caption">
+					<h3>Cannon EOS </h3>
+					<p>Price: Rs. 36000.00 </p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 1 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=1" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/2.jpg" alt="">
+				<div class="caption">
+					<h3>Samsung EOS </h3>
+					<p>Price: Rs. 40,000.00 </p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 2 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=2" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/3.jpg" alt="">
+				<div class="caption">
+					<h3>Sony DSLR</h3>
+					<p>Price: Rs. 50000.00</p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 3 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=3" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-3 col-sm-6 home-feature">
+			<div class="thumbnail">
+				<img src="img/4.jpg" alt="">
+				<div class="caption">
+					<h3>Olympus DSLR</h3>
+					<p>Price: Rs. 50000.00</p>
+					<?php if (!isset($_SESSION['email'])) { ?>
+					<p><a href="login.php" role="button" class="btn btn-primary btn-block">Apply Now</a>
+					</p>
+					<?php
+					} else {
+						//We have created a function to check whether this particular product is added to cart or not.
+						if ( check_if_added_to_cart( 4 ) ) { //This is same as if(check_if_added_to_cart != 0)
+							echo '<a href="#" class="btn btn-block btn-success" disabled>Added to cart</a>';
+						} else {
+							?>
+					<a href="cart-add.php?id=4" name="add" value="add" class="btn btn-block btn-primary">Add to cart</a>
+					<?php
+					}
+					}
+					?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+
+	</div>
 
 
 	<?php 
 		include("includes/footer.php"); 
 	?>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
+	<script type="text/javascript">
+		$( "#Login-toggle" ).click( function () {
+			$( "#login-pane" ).toggle();
+			$( ".overlay" ).toggle();
+		} );
+		$( ".login-close" ).click( function () {
+			$( "#login-pane" ).toggle();
+			$( ".overlay" ).toggle();
+		} );
+
+		$( "#std" ).click( function () {
+			$( "#std" ).addClass( "active" );
+			$( "#emp" ).removeClass( "active" );
+			$( "#login-email" ).attr( "placeholder", "Student email" );
+		} );
+
+		$( "#emp" ).click( function () {
+			$( "#std" ).removeClass( "active" );
+			$( "#emp" ).addClass( "active" );
+			$( "#login-email" ).attr( "placeholder", "Employee email" );
+		} );
+	</script>
 </body>
 
 </html>
