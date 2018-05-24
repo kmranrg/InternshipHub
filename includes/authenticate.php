756dc6b31f611a418a0ad782e46cdeb2e42444c0
Kumar Anurag <kmranrg@gmail.com>
2018-05-24 10:11:31 +0530

    applied table  data insertion

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -5,16 +5,42 @@ function authentication() {
 	$password = $_POST[ "password" ];
 	if ( isset( $_POST[ "signin" ] ) ) {
 		$dblink = mysqli_connect( "localhost", "root", "", "internships" );
-		$qry = "select * from employee where email='" . $email . "' and password='" . $password . "'";
-		$res = mysqli_query( $dblink, $qry );
-		$row = mysqli_fetch_assoc( $res );
-		if ( $row != NULL ) {
+		$emp_qry = "select * from employee where email='" . $email . "' and password='" . $password . "'";
+		$stu_qry = "select * from student where email='" . $email . "' and password='" . $password . "'";
+		$emp_res = mysqli_query( $dblink, $emp_qry );
+		$stu_res = mysqli_query( $dblink, $stu_qry );
+		$emp_row = mysqli_fetch_assoc( $emp_res );
+		$stu_row = mysqli_fetch_assoc( $stu_res );
+		if ( $emp_row != NULL ) {
 			session_start();
-			$_SESSION[ "email" ] = $row[ 'email' ];
-			header( "Location:emp_profile.php" );
+			$_SESSION[ "email" ] = $emp_row[ 'email' ];
+			header( "Location:emp_dashboard.php" );
+		} else if ( $stu_row != NULL ) {
+			session_start();
+			$_SESSION[ "email" ] = $stu_row[ 'email' ];
+			header( "Location:stu_dashboard.php" );
 		} else {
 			echo '<script>window.alert("Either username or password is incorrect (or not filled)");</script>';
 		}
 	}
 }
+
+function stu_authentication() {
+	$email = $_POST[ "email" ];
+	$password = $_POST[ "password" ];
+	if ( isset( $_POST[ "signin" ] ) ) {
+		$dblink = mysqli_connect( "localhost", "root", "", "internships" );
+		$stu_qry = "select * from student where email='" . $email . "' and password='" . $password . "'";
+		$stu_res = mysqli_query( $dblink, $stu_qry );
+		$stu_row = mysqli_fetch_assoc( $stu_res );
+		if ( $stu_row != NULL ) {
+			session_start();
+			$_SESSION[ "id" ] = $stu_row[ 'id' ];
+			header( "Location:internship_details.php" );
+		} else {
+			echo '<script>window.alert("Either username or password is incorrect (or not filled)");</script>';
+		}
+	}
+}
+
 ?>
